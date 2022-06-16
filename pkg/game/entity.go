5a2b9f58a50a2ce0367d6f4f6c8fde436b3f3d38
kts of kettek <kts@kettek.net>
2022-06-15 18:55:36 -0700

    Add entity interface and base type

---
+++ pkg/game/entity.go
@@ -0,0 +1,27 @@
+package game
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+type Entity interface {
+	Physics() *PhysicsObject
+	Trashed() bool
+	Update() error
+	Draw(screen *ebiten.Image) // Eh, might as well allow the entities to draw themselves.
+}
+
+type BaseEntity struct {
+	physics PhysicsObject
+	trashed bool
+}
+
+func (e *BaseEntity) Physics() *PhysicsObject {
+	return &e.physics
+}
+
+func (e *BaseEntity) Trashed() bool {
+	return e.trashed
+}
+
+func (e *BaseEntity) Trash() {
+	e.trashed = true
+}
