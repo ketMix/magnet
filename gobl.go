a97e9435736a35819d059ce92e78aea49248ae08
kts of kettek <kts@kettek.net>
2022-06-23 07:18:14 -0700

    Add host and join tasks

--- gobl.go
+++ gobl.go
@@ -25,6 +25,23 @@ func main() {
 		Watch("cmd/*/*", "cmd/*/*/*", "pkg/*/*", "pkg/*/*/*", "pkg/*/*/*/*").
 		Run("build")
 
+	Task("host").
+		Watch("./magnet" + exe).
+		Signaler(SigQuit).
+		Run("runServer")
+
+	Task("join").
+		Watch("./magnet" + exe).
+		Signaler(SigQuit).
+		Run("runClient")
+
+	Task("runServer").
+		Exec("./magnet"+exe, "-n", "server", "--host", ":9999")
+
+	Task("runClient").
+		Sleep("500ms").
+		Exec("./magnet"+exe, "-n", "client", "--join", "localhost:9999")
+
 	Task("run").
 		Exec("./magnet" + exe)
 
