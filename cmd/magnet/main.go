a4b5806919c90b9dd98bdbff74604310ce8ec281
kts of kettek <kts@kettek.net>
2022-06-29 21:47:13 -0700

    Force OpenGL for graphics backend

--- cmd/magnet/main.go
+++ cmd/magnet/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/kettek/ebijam22/pkg/game"
 	"github.com/thought-machine/go-flags"
@@ -12,6 +15,10 @@ func main() {
 	// Parse our initial command-line derived options.
 	if _, err := flags.Parse(&g.Options); err != nil {
 		return
+	}
+
+	if err := os.Setenv("EBITEN_GRAPHICS_LIBRARY", "opengl"); err != nil {
+		fmt.Println("WARNING: OpenGL backend could not be set, expect degraded performance if on DirectX.")
 	}
 
 	if err := g.Init(); err != nil {
